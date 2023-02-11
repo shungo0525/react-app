0a7c332ec5dac0a505cbf0e799c54fd40407f00e
shungo0525 <shungo0525@gmail.com>
2023-02-11 22:09:01 +0900

    suspenseを使用

--- pages/top.tsx
+++ pages/top.tsx
@@ -1,11 +1,35 @@
 import styled from 'styled-components'
+import {Suspense, VFC, useState} from "react"
+import {sleep} from "../lib"
 
 const Top = () => {
   return (
     <>
       <Container>
         <h2>Top</h2>
+        <Description />
       </Container>
+    </>
+  )
+}
+
+export const SometimesSuspend: VFC = () => {
+  if (Math.random() < 0.5) {
+    throw sleep(100)
+  }
+  return <p>Hello, world!</p>
+}
+
+const Description = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <Suspense fallback={<p>Loading...</p>}>
+        <SometimesSuspend />
+        <button className="border p-1" onClick={() => setCount((c) => c + 1)}>
+          {count}
+        </button>
+      </Suspense>
     </>
   )
 }
