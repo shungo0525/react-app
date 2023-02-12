fe29fbbc1110f514affa8f634abfad8afbe6fbb7
shungo0525 <shungo0525@gmail.com>
2023-02-12 09:48:59 +0900

    404 page 作成

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Container>
+        404 Not Found
+      </Container>
+    </>
+  )
+
+}
+
+export default ErrorPage
+
+const Container = styled.div`
+  text-align: center;
+`
